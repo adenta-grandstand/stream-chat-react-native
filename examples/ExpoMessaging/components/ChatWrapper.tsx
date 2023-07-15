66d1ea5f799d5e7989d1588b57636d9f50c34886
Andrew Denta <andrew.denta@gmail.com>
2023-07-14 20:57:51 -0400

    update app to autoplay

--- examples/ExpoMessaging/components/ChatWrapper.tsx
+++ examples/ExpoMessaging/components/ChatWrapper.tsx
@@ -1,10 +1,10 @@
 import React, { PropsWithChildren } from 'react';
-import { Chat, OverlayProvider, Streami18n } from 'stream-chat-expo';
+import { Chat, OverlayProvider, Streami18n, registerNativeHandlers } from 'stream-chat-expo';
 import { useChatClient } from '../hooks/useChatClient';
 import { AuthProgressLoader } from './AuthProgressLoader';
 import { StreamChatGenerics } from '../types';
 import { STREAM_API_KEY, user, userToken } from '../constants';
-
+import { Video } from 'expo-av';
 const streami18n = new Streami18n({
   language: 'en',
 });
@@ -15,6 +15,33 @@ export const ChatWrapper = ({ children }: PropsWithChildren<{}>) => {
     userData: user,
     tokenOrProvider: userToken,
   });
+
+  React.useEffect(() => {
+    registerNativeHandlers({
+      Video: (props) => {
+        const { onPlaybackStatusUpdate, paused, resizeMode, style, uri, videoRef, onLoad } = props;
+        const [touched, setTouched] = React.useState<boolean>(false);
+        return (
+          <Video
+            onPlaybackStatusUpdate={(props) => {
+              if (props.isLoaded && paused && !touched) {
+                videoRef.current.setStatusAsync({ shouldPlay: true });
+                setTouched(true);
+              }
+              onPlaybackStatusUpdate(props);
+            }}
+            ref={videoRef}
+            resizeMode={resizeMode}
+            shouldPlay={!paused}
+            source={{
+              uri,
+            }}
+            style={[style]}
+          />
+        );
+      },
+    });
+  }, []);
 
   if (!chatClient) {
     return <AuthProgressLoader />;
